c13cf998a86b0fc71afa6fa42e5b92420355a126
FranckBisschop <bisschopfranck@gmail.com>
2025-01-12 16:29:01 +0100

    chore: install dependencies gluestack and create first component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { StyleSheet, Text, View } from 'react-native';
+import { Heading } from './components/ui/heading';
+import { Box } from './components/ui/box';
 
 export default function App() {
   return (
-    <GluestackUIProvider mode="light"><View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
+    <GluestackUIProvider mode="light">
+      <View style={styles.container}>
+        <Heading>Seraya Mobile</Heading>
+        <Text>Seraya Mobile</Text>
         <StatusBar style="auto" />
-      </View></GluestackUIProvider>
+      </View>
+      <View>
+        <Box className="bg-primary-500 p-5">
+          <Text className="text-typography-0">This is the Box</Text>
+        </Box>
+      </View>
+    </GluestackUIProvider>
   );
 }
 
